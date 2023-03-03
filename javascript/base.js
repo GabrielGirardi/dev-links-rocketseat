fc0995807ad0de88c4a1ddf3f1478d476033b98c
Gabriel Girardi <gg@MacBook-Pro.local>
2023-03-03 20:55:30 -0300

    docs: ✏️ Removido comentários inúteis

--- javascript/base.js
+++ javascript/base.js
@@ -1,21 +1,3 @@
-// $(document).ready(function() {	
-// 	let themeButton = $('.theme-button');
-// 	let theme = $('html');
-// 	let darkMode = ('<link href="./scss/dark-theme.css" rel="stylesheet" id="darkMode">');
-// 	
-// 	$(themeButton).on('click', function() {
-// 		$(this).toggleClass('dark');
-// 		$(theme).data('data-theme', 'dark');
-// 		$(theme).attr('data-theme', ($(theme).attr('data-theme') == 'dark' ? 'light' : 'dark'));
-// 		if(theme.attr('data-theme') == 'dark' ){
-// 			$('head').append(darkMode)
-// 		} else {
-// 			$('head link#darkMode').remove();
-// 		}
-// 	});
-// });
-
-
 $(document).ready(function() {	
 	let themeButton = $('.theme-button');
 	let themeSelector = $('#themeSelector');
